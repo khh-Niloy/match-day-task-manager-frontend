a40fae22fd4b08d5660a81c6d6b6e42975e53f87
khh-Niloy <khhniloy0@gmail.com>
2026-02-07 12:59:19 +0600

    feat: Add create task functionality using shadcn/ui components, form handling, and Redux integration.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <Toaster position="top-center" reverseOrder={false} />
+      <App />
+    </Provider>
   </StrictMode>,
-)
+);
